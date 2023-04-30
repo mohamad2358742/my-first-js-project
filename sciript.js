015aa364e72d9df82d8b203e272b909f7152169b
mohammad <cat.dog8299@gmail.com>
2023-05-01 00:38:01 +0200

    1554

--- sciript.js
+++ sciript.js
@@ -1,5 +1,7 @@
 hi dont forget sub and like for more video 
 first to hit on ctrl +`  like this 
-when is green its meen we add code on panel  and now is red becuase we must be add in panel like this with git add . command now is add
+when is green its mean we add code on panel  and now is red becuase we must be add in panel like this with git add . command now is add
 im sorry
 retry
+now is green its mean we add in panel now get git commit -m'name of file
+now just get punish im sorry get push 
